7bb936111d8856a4d27c600bf84e65b379c95cb6
Kainatfatima711 <kainatfetima@gmail.com>
2025-12-28 14:23:17 +0500

    Add files via upload

---
+++ OneAlgoThreeFaces.py
@@ -0,0 +1,17 @@
+def fun1(n):
+    return n*(n+1)/2
+
+def fun2(n):
+    sum = 0
+    for i in range(1 , n + 1):
+        sum = sum + i
+    return sum
+
+def fun3(n):
+    sum = 0
+    for i in range (1 , n + 1):
+        for j in range (i , i + 1):
+            sum = sum + j
+        return sum
+    
+print(fun3(4))
