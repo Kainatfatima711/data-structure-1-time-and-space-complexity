63a492d195947ff2f8d8df3043ed56bd56fb6ec5
Kainatfatima711 <kainatfetima@gmail.com>
2026-01-03 14:11:02 +0500

    Add files via upload

---
+++ O(1)Time.py
@@ -0,0 +1,12 @@
+def print_number(n):
+    iteration = 0
+
+    iteration = iteration + 1
+
+    print("Total iterations done by the code is: " , iteration)
+    print()
+
+print_number(10)
+print_number(20)
+
+print("With any value of 'n', the time taken by our code will not change")
